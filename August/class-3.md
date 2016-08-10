7cc2c1d5c5372075790b1192c3a70edeecf09693
= <nncl@live.com>
2016-08-09 21:18:16 -0300

    Class 3 - requesting route w/ parameters via CLI.

---
+++ August/class-3.md
@@ -0,0 +1,15 @@
+# Class 3
+
+Working w/ **GET** verb.
+
+Exercise w/ Hello World Querystring.js, sending a request and parameters via CLI.
+
+- GET() different than REQUEST().
+- Income Message
+    - close
+    - data
+    - end
+    - error
+
+## REQUEST
+
---
+++ August/nodejs/class-3/class_3_get_localhost_querystring.js
@@ -0,0 +1,28 @@
+/**
+ * Making a GET request and displaying it
+ *
+ * ES6
+ */
+
+'use strict';
+
+const http = require('http');
+
+http.get({
+    hostname: 'localhost',
+    path: '/user?name=Caue&teacher=false&age=21',
+    port: 3000,
+    agent: false
+}, (response) => {
+    let body = ";";
+    console.log('STATUS', response.statusCode);
+    console.log('HEADERS', response.headers);
+
+    response.on('data', function (data) {
+        body += data;
+    });
+
+    response.on('end', function () {
+        console.log('response', body)
+    });
+});
